c3d03a2aa610dab6407f0066aa5108cb73572e45
Torben Haack <tohaack@pm.me>
2024-11-19 12:12:37 +0100

    metadata + create default user when it does not exist

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,21 @@
 import Header from "@/components/header";
 import "./globals.css";
+import { ensureDefaultUserExists } from "@/lib/utils";
+import type { Metadata } from "next";
 
 interface RootLayoutProps {
 	children: React.ReactNode;
 }
 
+export const metadata: Metadata = {
+	title: "petscale",
+	description: "Tracking weight for your pets",
+};
+
 export default function RootLayout(props: RootLayoutProps) {
 	const { children } = props;
+
+	ensureDefaultUserExists();
 
 	return (
 		<html lang="de">
