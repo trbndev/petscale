6b2f6dbd321f1ec21e4226bfe942ed3fe50880e0
Torben Haack <tohaack@pm.me>
2024-11-20 15:20:01 +0100

    add link to header

--- components/header.tsx
+++ components/header.tsx
@@ -3,11 +3,14 @@ import {
 	DeletePetDialog,
 	TrackWeightDialog,
 } from "@/components/dialogs";
+import Link from "next/link";
 
 export default function Header() {
 	return (
 		<header className="w-dvw px-8 py-4 border-b-2 border-stone-200 flex flex-row justify-between items-center bg-white">
-			<h1 className="font-bold text-lg">petscale.</h1>
+			<Link href="/">
+				<h1 className="font-bold text-lg">petscale.</h1>
+			</Link>
 			<div className="flex gap-4 justify-center items-center h-auto">
 				<TrackWeightDialog />
 				<CreatePetDialog />
